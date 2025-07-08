97403783908c011bed9006aed6126a9ccbf658e6
Martin Fernandez <em.fernandez@outlook.com>
2025-07-07 20:12:27 -0400

    pytest example

---
+++ test_example.py
@@ -0,0 +1,11 @@
+import re
+from playwright.sync_api import Page, expect
+
+def test_has_title(page: Page):
+    page.goto("https://playwright.dev/")
+    expect(page).to_have_title(re.compile("Playwright"))
+
+def test_get_started(page: Page):
+    page.goto("https://playwright.dev/")
+    page.get_by_role("link", name="Get started").click()
+    expect(page.get_by_role("heading", name="installation")).to_be_visible()
